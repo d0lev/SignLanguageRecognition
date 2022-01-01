d3338bd1baaccf2c4f2ff3bb879c9129ea3eff8d
Egor_Kononov <egor.kononov43@gmail.com>
2022-01-02 00:36:02 +0200

    added plot filters and features map

--- ConvolutionalNeuralNetwork.py
+++ ConvolutionalNeuralNetwork.py
@@ -7,6 +7,7 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 from keras import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D , Dropout
+from keras.models import Model
 
 
 class ConvolutionalNeuralNetwork:
@@ -76,3 +77,52 @@ class ConvolutionalNeuralNetwork:
         plt.ylabel('Fraction of incorrect predictions')
         plt.xticks(np.arange(25), self.data.labels)
         plt.show()
+
+    def plot_filters(self):
+        filters, biases = self.cnn.layers[6].get_weights()
+        fig1 = plt.figure(figsize=(8, 12))
+        n_filters = 128
+        for i in range(1, n_filters + 1):
+            f = filters[:, :, :, i - 1]
+            fig1 = plt.subplot(8, 16, i)
+            fig1.set_xticks([])  # Turn off axis
+            fig1.set_yticks([])
+            plt.imshow(f[:, :, 0], cmap='gray')  # Show only the filters from 0th channel (R)
+            # ix += 1
+        plt.show()
+
+    def plot_features_maps(self):
+        layer = self.cnn.layers
+        img = self.x_test[5].reshape(1, 28, 28, 1)
+        layer_outputs = [layer.output for layer in self.cnn.layers]
+        activation_model = Model(inputs=self.cnn.input, outputs=layer_outputs)
+        activations = activation_model.predict(img)
+        layer_names = []
+        for layer in self.cnn.layers[0:6]:
+            layer_names.append(layer.name)  # Names of the layers, so you can have them as part of your plot
+
+        images_per_row = 16
+        for layer_name, layer_activation in zip(layer_names, activations):  # Displays the feature maps
+            n_features = layer_activation.shape[-1]  # Number of features in the feature map
+            size = layer_activation.shape[1]  # The feature map has shape (1, size, size, n_features).
+            n_cols = n_features // images_per_row  # Tiles the activation channels in this matrix
+            display_grid = np.zeros((size * n_cols, images_per_row * size))
+            for col in range(n_cols):  # Tiles each filter into a big horizontal grid
+                for row in range(images_per_row):
+                    channel_image = layer_activation[0,
+                                    :, :,
+                                    col * images_per_row + row]
+                    channel_image -= channel_image.mean()  # Post-processes the feature to make it visually palatable
+                    channel_image /= channel_image.std()
+                    channel_image *= 64
+                    channel_image += 128
+                    channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+                    display_grid[col * size: (col + 1) * size,  # Displays the grid
+                    row * size: (row + 1) * size] = channel_image
+            scale = 1. / size
+            plt.figure(figsize=(scale * display_grid.shape[1],
+                                scale * display_grid.shape[0]))
+            plt.title(layer_name)
+            plt.grid(False)
+            plt.imshow(display_grid, aspect='auto', cmap='viridis')
+            plt.show()
